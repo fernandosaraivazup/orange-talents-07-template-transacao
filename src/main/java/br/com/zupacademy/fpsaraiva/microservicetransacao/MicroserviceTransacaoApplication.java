6d2eafef31d6706cce1023ba67e17b979cf5df47
Fernando Saraiva <fernando.saraiva@zup.com.br>
2021-09-14 11:55:25 -0300

    [4.13.2] add setup do projeto

---
+++ src/main/java/br/com/zupacademy/fpsaraiva/microservicetransacao/MicroserviceTransacaoApplication.java
@@ -0,0 +1,13 @@
+package br.com.zupacademy.fpsaraiva.microservicetransacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceTransacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceTransacaoApplication.class, args);
+	}
+
+}
